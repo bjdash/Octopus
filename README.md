4085b3b75b89458c3e134f5b3f62a6cff6a40d5a
Bijaya <bijaya.k.dash@gmail.com>
2026-08-17 15:01:57 +0100

    created backup branch - reset

--- README.md
+++
@@ -1,5 +0,0 @@
-# Octopus
-
-A simple app to better visualize the Agile tariff for [Octopus energy](https://octopus.energy).
-
-# visit https://bjdash.github.io/Octopus/
--- githubPages.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-fs.copyFile('docs/index.html', 'docs/404.html', (err) => {
-  if (err) {
-    console.error('index.html -> 404.html copy failed:');
-    throw err;
-  }
-});
